fdac86266c3d1bf20d251c218652a117ba9eebe1
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-11-09 16:33:37 +0530

    added dummy route for hosting test

--- src/index.js
+++ src/index.js
@@ -17,5 +17,3 @@ const initializeExpress = () => {
 };
 
 initializeExpress();
-
-export { app };
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import * as authentication from "./controllers/authentication.js";
 
 export const Routes = (app) => {
   // Authentication Routes
+  app.get("/", (req, res) => res.send("Hello from the Getlyst Server"));
   app.get("/login", authentication.login);
   app.get("/callback", authentication.callback);
   app.get("/refresh-token", authentication.token_refresh);
